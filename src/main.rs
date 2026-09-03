e288dca9ecca84beb67fc71509bd06d55e7c82f1
haniissa <zaraki2011@gmail.com>
2026-09-03 21:49:46 +0300

    add: simple command line

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+/// Search for a pattern in a file and display the lines that contains it.
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    ///The path to the file to read
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let pattern = std::env::args().nth(1).expect("no pattern given");
+    let path = std::env::args().nth(2).expect("no path given");
+
+    let args = Cli {
+        pattern,
+        path: std::path::PathBuf::from(path),
+    };
+    println!("pattern: {:?}, path:{:?}", args.pattern, args.path);
+}
