5edfe55554da96f320e680c0aa2947afa030cd69
haniissa <zaraki2011@gmail.com>
2026-09-04 06:52:36 +0300

    add: clap parser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+
+
+use clap::Parser;
+
 /// Search for a pattern in a file and display the lines that contains it.
+#[derive(Parser)]
 struct Cli {
     /// The pattern to look for
     pattern: String,
@@ -7,12 +12,19 @@ struct Cli {
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+    let args = Cli::parse();
+    // let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let file = File::open(&args.path).expect("file not found");
+    // let file = File::open(&args.path)
+    //     .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+    let content = BufReader::new(file);
 
-    let args = Cli {
-        pattern,
-        path: std::path::PathBuf::from(path),
-    };
-    println!("pattern: {:?}, path:{:?}", args.pattern, args.path);
+    for line in content.lines() {
+        let line = line.expect("could not read line");
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+    // println!("pattern: {:?}, path:{:?}", args.pattern, args.path);
+    // Ok(())
 }
