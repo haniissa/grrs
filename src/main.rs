86b3e0ef4fc5323aa5ab93b65f7b41a3c586f5c5
haniissa <zaraki2011@gmail.com>
2026-09-10 00:44:03 +0300

    feat: implement file pattern searching logic with CLI support and integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use anyhow::{Context, Ok, Result};
+use anyhow::{Context, Result};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 use clap::Parser;
+
+//my own library
+mod lib;
+// use crate::lib;
 
 /// Search for a pattern in a file and display the lines that contains it.
 #[derive(Parser)]
@@ -18,13 +22,6 @@ fn main() -> Result<()> {
     let file = File::open(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
     let content = BufReader::new(file);
-
-    for line in content.lines() {
-        //Unpack the result to ge the actual String
-        let line = line?;
-        if line.contains(&args.pattern) {
-            println!("{:?}", line);
-        }
-    }
+    grrs::find_matches(content, &args.pattern, &mut std::io::stdout());
     Ok(())
 }
